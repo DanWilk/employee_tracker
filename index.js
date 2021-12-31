12eecbf167a29db5b91d6dd9da0d259ae5ff1738
DanWilk <dcwilkinson@wilktech.com>
2021-12-31 18:15:23 -0500

    a

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const { title } = require('process');
+const { resolve } = require('path');
 
 const db = mysql.createConnection(
     {
@@ -11,20 +13,30 @@ const db = mysql.createConnection(
         password: 'Dan$0430',
         database: 'employee_tracker'
     },
-    console.log('Connected to the election database')
+    console.log('Connected to the employee_tracker database')
 );
 
 const viewAllDepartments = () => {
-    const sql = `SELECT * FROM departments`;
+    
+    new Promise((resolve, reject) => {
+        const sql = `SELECT * FROM departments`;
 
-    db.query(sql, (err, results) => {
-          console.table(results);
-        }
-    );
-}
+        db.query(sql, (err, results) => {
+              console.table(results);
+            }
+        );
+        console.log();
+        resolve(true);
+    })
+    .then(promptUser);
+};
 
 const viewAllRoles = () => {
-    const sql = `SELECT * FROM roles`;
+    console.log('f');
+    const sql = `select roles.title, roles.salary, departments.department_name
+    from roles
+    join departments
+        on roles.department_id = departments.id;`;
 
     db.query(sql, (err, results) => {
           console.table(results);
@@ -37,13 +49,108 @@ const viewAllEmployees = () => {
 
     db.query(sql, (err, results) => {
           console.table(results);
+          console.log('');
         }
     );
 }
 
+const addEmployee = () => {
+    return  inquirer.prompt([
+        {
+            type: 'input',
+            name: 'new_department',
+            message: 'What is the new departments name',
+        }
+    ])
+    .then(data => {
+        const sql = `INSERT INTO departments (department_name)
+        VALUES (?)`;
+
+        db.query(sql, data.new_department, (err, results) => {
+            viewAllDepartments();
+            }
+        );
+    });
+}
+
+// const addRole = () => { 
+//     const getDepartments = () => {
+//         db.query(`SELECT department_name FROM departments`, (err, rows) => {
+//             return rows[1];
+//     });
+//     }
+
+//     console.log(getDepartments());
+//     return  inquirer.prompt([
+//         {
+//             type: 'input',
+//             name: 'new_role',
+//             message: 'What is the new roles name'
+//         },
+//         {
+//             type: 'list',
+//             name: 'new_role_department',
+//             message: 'What department is the new role in',
+//             choices: [getDepartments()]
+//         },
+//         {
+//             type: 'input',
+//             name: 'new_role_salary',
+//             message: 'What is the new roles salary'
+//         }
+
+//     ])
+//     .then(data => {
+//         const sql = `INSERT INTO roles (title, salary, department_id)
+//         VALUES (?,?,?)`;
+
+//         db.query(sql, [data.new_role, data.new_role_salary, data.new_role_department], (err, results) => {
+//             viewAllRoles();
+//             }
+//         );
+//     })
+//     .then(() => {
+//         promptUser();
+//     });
+// }
 
 const actionFilter = ({ action }) => {
-    if (data.a)
+    if (action === 'view all departments') {
+       viewAllDepartments();
+    } else if (action === 'view all roles') {
+        viewAllRoles();
+        promptUser();
+    } else if (action === 'view all employees') {
+        viewAllEmployees();
+        promptUser();
+    } else if (action === 'add a department') {
+        addDepartment();
+    } else if (action === 'add a role') {
+        addRole();
+    }
+    // promptUser()
+}
+
+const addDepartment = () => {
+    return  inquirer.prompt([
+        {
+            type: 'input',
+            name: 'new_department',
+            message: 'What is the new departments name',
+        }
+    ])
+    .then(data => {
+        const sql = `INSERT INTO departments (department_name)
+        VALUES (?)`;
+
+        db.query(sql, data.new_department, (err, results) => {
+            viewAllDepartments();
+            }
+        );
+    })
+    .then(() => {
+        promptUser();
+    });
 }
 
 const promptUser = () => {
@@ -62,14 +169,24 @@ const promptUser = () => {
                 'update employee role'
             ]
         }
-    ]);
+    ])
+    .then(data => {
+        console.log('');
+        return data;
+    })
+    .then(data => {
+        actionFilter(data);
+    })
 }
 promptUser()
-.then(data => {
-    if (data.action === 'view all departments') {
-        viewAllDepartments()
-    }
-    console.log(data);
-});
 
 
+
+// const sql = `SELECT * FROM roles`;
+
+//     db.query(sql, (err, rows) => {
+//           rows.forEach((value, index) => {
+//             console.log(rows[index].title)
+//           });
+//         }
+//     );
